68e514bc2bdc9aa945deea9ece76a03090c0e7b6
Özge Kurnaz <ozge.k60@hotmail.com>
2023-12-14 02:31:28 +0300

    Timer sınıfı eklendi.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,26 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // zamanlamaları ekle
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    let eggTimes  = ["Soft" : 10, "Medium":420, "Hard":720]
     
+    var seconds = 60
+    var timer = Timer()
+    
+    
+    @IBOutlet weak var textLabel: UILabel!
     
     @IBAction func hardnessSelecter(_ sender: UIButton) {
         
+        timer.invalidate() // bir işlem sırasında tekrardan diger butona o işlemi durdurup digerini baslatmak icin
         let hardness = sender.currentTitle!
         
-        if hardness == "Soft"{
-            print(softTime)
-        }
-        else if hardness == "Medium"{
-            print(mediumTime)
-        }
-        else {
-            print(hardTime)
-        }
+        seconds = eggTimes[hardness]!
+        
+        // belli zaman aralıgında tekrarlanan işlemler
+        timer = Timer.scheduledTimer(
+        timeInterval: 1.0,
+        target: self,
+        selector: #selector(updateCounter),
+        userInfo: nil,
+        repeats: true)
+        
+        
     }
     
-
+    @objc func updateCounter(){
+        if seconds>0{
+            print("\(seconds) seconds")
+            seconds -= 1
+        }
+        else{
+            timer.invalidate()
+            textLabel.text = "DONE!"
+        }
+    }
 }
+
