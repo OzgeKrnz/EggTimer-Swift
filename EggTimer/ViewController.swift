5f89b703605f0866894cd4b60e25a621c9ecf756
Özge Kurnaz <ozge.k60@hotmail.com>
2025-08-04 17:05:20 +0300

    Applied Single Responsibility Principle to isolate concerns. Timer and audio playback are now handled by `EggTimer` and `AudioPlayerService`.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,60 +1,97 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController {
-    
+
+protocol EggTimerDelegate: AnyObject{
+    func eggTimerDidUpdate(progress: Float, currentText: String)
+    func eggTimerDidFinish(endText: String)
+}
+
+
+class EggTimer{
+    weak var delegate: EggTimerDelegate?
+    //SRP
     let eggTimes  = ["Soft" : 3, "Medium":4, "Hard":7]
     
     var timer = Timer()
     var totalTime = 0
     var secondsPassed = 0
     
-    var player: AVAudioPlayer!
     
-    @IBOutlet weak var textLabel: UILabel!
-    
-    @IBOutlet weak var progressBar: UIProgressView!
-    
-    
-    @IBAction func hardnessSelecter(_ sender: UIButton) {
-        
-    
+    func startTimer(for hardness:String){
         timer.invalidate() // bir işlem sırasında tekrardan diger butona o işlemi durdurup digerini baslatmak icin
-        let hardness = sender.currentTitle!
-        totalTime = eggTimes[hardness]!
         
-        progressBar.progress = 0.0
+        guard let time = eggTimes[hardness] else {return}
+        totalTime = time
         secondsPassed = 0
-        textLabel.text = hardness
+
         // belli zaman aralıgında tekrarlanan işlemler
         timer = Timer.scheduledTimer(
         timeInterval: 1.0,
         target: self,
-        selector: #selector(updateCounter),
+        selector: #selector(update),
         userInfo: nil,
         repeats: true)
         
         
     }
     
-    @objc func updateCounter(){
-        if secondsPassed < totalTime{
+    @objc private func update() {
+        if secondsPassed < totalTime {
             secondsPassed += 1
-
-            progressBar.progress = Float(secondsPassed) / Float(totalTime)
-        }
-        else{
+            let progress = Float(secondsPassed) / Float(totalTime)
+            delegate?.eggTimerDidUpdate(progress: progress, currentText: "...") // İsteğe bağlı metin
+        } else {
             timer.invalidate()
-            textLabel.text = "DONE!"
-            playSound(soundName: "alarm_sound")
+            delegate?.eggTimerDidFinish(endText: "DONE!")
         }
     }
+    
+}
+
+class ViewController: UIViewController {
+    
+    private let eggTimer = EggTimer()
+    private let player = SoundPlayer()
+        
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        eggTimer.delegate = self
+    }
+    
+    @IBAction func hardnessSelecter(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle!
+        progressBar.progress = 0.0
+        eggTimer.startTimer(for: hardness)
+        
+    }
+    
+}
+
+class SoundPlayer{
+    var player: AVAudioPlayer!
     
     func playSound(soundName: Optional<String>) {
         let url = Bundle.main.url(forResource:soundName, withExtension: "mp3")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
+        
     }
 }
-
+extension ViewController: EggTimerDelegate {
+    
+    func eggTimerDidUpdate(progress: Float, currentText: String) {
+        // Gelen veriye göre SADECE UI'ı güncelliyor.
+        progressBar.progress = progress
+    }
+    
+    func eggTimerDidFinish(endText: String) {
+        // Gelen veriye göre SADECE UI'ı güncelliyor ve diğer servisi çağırıyor.
+        textLabel.text = endText
+        player.playSound(soundName: "alarm_sound")
+    }
+}
